c1470717fe80bfa07e45828859419eda5eb791d5
Gabriel Gonçalves Martins <gabrielgmartinsbr@gmail.com>
2024-04-22 17:31:43 -0300

    Change board

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -70,7 +70,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_MKS_ROBIN_E3D
+  #define MOTHERBOARD BOARD_MKS_ROBIN_E3D_V1_1
 #endif
 
 #define X_MAX_PIN  3 // Creality connects X_MAX to X_MIN(_PIN) motherboard connector
